5ae0864eece9f111d4b5a6fc87986a1da9730064
Arsen Aleksanyan <arsen.aleksanyan.1994@gmail.com>
2021-12-12 15:04:15 +0400

    Refactor components

--- src/weather/card.js
+++ src/weather/card.js
@@ -7,7 +7,7 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function WeatherCard({ city = '', main, sys, weather }) {
+function WeatherCard({ city = '', main, sys }) {
   return (
     <div className="weather-card">
       <Card>
@@ -18,18 +18,13 @@ function WeatherCard({ city = '', main, sys, weather }) {
             <p className="day">{dayjs().format('DD-MM-YYYY')}</p>
           </div>
           <div className="flex">
-            <p className="temp">Temprature: {main.temp} &deg;C</p>
+            <p className="temp">temperature: {main.temp} &deg;C</p>
             <p className="temp">Humidity: {main.humidity} %</p>
           </div>
-
           <div className="flex">
-            <p className="sunrise-sunset">Sunrise: {new Date(sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-            <p className="sunrise-sunset">Sunset: {new Date(sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+            <p className="sunrise-sunset">Sunrise: {dayjs(sys.sunrise * 1000).format('hh:mm:ss a')}</p>
+            <p className="sunrise-sunset">Sunset: {dayjs(sys.sunset * 1000).format('hh:mm:ss a')}</p>
           </div>
-
-          {/* <p>Sunrise: {new Date(sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-          <p>Sunset: {new Date(sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-          <p>Description: {weather[0].main}</p> */}
         </CardContent>
       </Card>
       </div>
--- src/weather/index.js
+++ src/weather/index.js
@@ -6,30 +6,40 @@ import WeatherCard from './card';
 const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 function Weather() {
-  const [currentCity, setCurrentCity] = useState('');
   const [weather, setWeather] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentCity, setCurrentCity] = useState('');
+
+  const handleSearch = value => {
+    setWeather(null);
+    setIsLoading(true);
+    setCurrentCity(value)
+  }
 
   useEffect(() => {
     if (!currentCity) return;
 
-    async function fetchCurrentCountry() {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${weatherApiKey}&units=metric`);
-      const data = await response.json();
+    setIsLoading(true);
 
-      setWeather(data);
-      console.log(data);
+    async function fetchCurrentCountry() {
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${weatherApiKey}&units=metric`);
+        const data = await response.json();
+  
+        setWeather(data);
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     fetchCurrentCountry()
   }, [currentCity])
 
-  console.log(currentCity, ':::: search')
-
   return (
     <div className="Weather">
       <h1>Weather App</h1>
-      <Search onSubmit={setCurrentCity}/>
-      {weather && (weather.cod !== '404' ? <WeatherCard city={currentCity} {...weather}/> : 'City not found')}
+      <Search onSubmit={handleSearch}/>
+      {!isLoading && (weather.cod !== '404' ? <WeatherCard city={currentCity} {...weather}/> : 'City not found')}
     </div>
   );
 }
